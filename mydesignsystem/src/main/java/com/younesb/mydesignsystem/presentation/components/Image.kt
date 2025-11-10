6dd7a2d0297a1f3e743e9685e97d6049bd73fa3a
Younes <younes.bouhouche12@gmail.com>
2025-11-10 18:01:08 +0100

    Renamed MyImage to Image

--- mydesignsystem/src/main/java/com/younesb/mydesignsystem/presentation/components/Image.kt
+++ mydesignsystem/src/main/java/com/younesb/mydesignsystem/presentation/components/Image.kt
@@ -2,12 +2,15 @@ package com.younesb.mydesignsystem.presentation.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularWavyProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,16 +19,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
 import kotlin.let
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
-fun MyImage(
+fun Image(
     model: Any?,
     icon: ImageVector?,
     modifier: Modifier = Modifier,
+    alt: (@Composable () -> Unit)? = null,
     iconTint: Color = MaterialTheme.colorScheme.onSurfaceVariant,
     shape: Shape = MaterialTheme.shapes.medium,
     background: Color = MaterialTheme.colorScheme.surfaceContainerLowest,
@@ -40,7 +46,13 @@ fun MyImage(
         modifier = modifier
             .clip(shape)
             .background(background)
-            .clickable(onClick != null) { onClick?.invoke() },
+            .then(
+                if (onClick != null) {
+                    Modifier.clickable(role = Role.Image) { onClick() }
+                } else {
+                    Modifier
+                }
+            ),
         contentScale = ContentScale.Crop,
         loading = {
             Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
@@ -48,14 +60,21 @@ fun MyImage(
             }
         },
         error = {
-            icon?.let {
-                Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(
+                Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                icon?.let {
                     Icon(
                         imageVector = it,
                         contentDescription = null,
                         modifier = Modifier.fillMaxSize(fraction),
                         tint = iconTint
                     )
+                }
+                ProvideTextStyle(MaterialTheme.typography.bodyMedium) {
+                    alt?.invoke()
                 }
             }
         },
