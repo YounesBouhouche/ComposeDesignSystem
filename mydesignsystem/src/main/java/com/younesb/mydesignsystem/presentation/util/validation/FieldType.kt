6558df0491f25b09d32b140806c0d6da879c33e1
Younes <younes.bouhouche12@gmail.com>
2025-11-10 22:26:15 +0100

    Add minLength and maxLength properties to FieldType data classes

--- mydesignsystem/src/main/java/com/younesb/mydesignsystem/presentation/util/validation/FieldType.kt
+++ mydesignsystem/src/main/java/com/younesb/mydesignsystem/presentation/util/validation/FieldType.kt
@@ -20,6 +20,8 @@ sealed class FieldType(
     )
     data object Email : FieldType(
         regex = "^[A-Za-z](.*)([@]{1})(.+)(\\.)(.+)",
+        minLength = 5,
+        maxLength = 254,
     )
     data class Password(
         override val minLength: Int = 8,
@@ -37,25 +39,38 @@ sealed class FieldType(
         },
     )
     data class PhoneNumber(val countryCode: String = "") : FieldType(
+        minLength = 7,
+        maxLength = 15 + countryCode.length,
         regex = if (countryCode.isNotEmpty()) {
             "^\\+${countryCode}[0-9]{7,15}$"
         } else {
             "^[0-9]{7,15}$"
         },
     )
-    data object Username : FieldType(
+    data class Username(
+        override val minLength: Int = 3,
+        override val maxLength: Int = 15
+    ) : FieldType(
+        minLength = minLength,
+        maxLength = maxLength,
         regex = "^[a-zA-Z0-9._-]{3,}$",
     )
 
     // Payment field types
     data object CreditCardNumber : FieldType(
         regex = "^[0-9]{13,19}$",
+        minLength = 13,
+        maxLength = 19,
     )
     data object CVV : FieldType(
         regex = "^[0-9]{3,4}$",
+        minLength = 3,
+        maxLength = 4,
     )
     data object ExpirationDate : FieldType(
         regex = "^(0[1-9]|1[0-2])\\/([0-9]{2})$",
+        minLength = 5,
+        maxLength = 5,
     )
 
     // Custom field type
